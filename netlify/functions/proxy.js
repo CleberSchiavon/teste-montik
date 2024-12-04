c6e7b9555f3c7836b8f5575374d96de70b58ea70
Cleber Henrique <cleberschiavon@outlook.com>
2024-12-03 23:59:04 -0300

    ci(app): adding netlify config files to deploy

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const response = await fetch('https://app.landingpage.com.br/api/checkoutloja/LPL2gc/5d87eb644e5631bc6a03f1e43a804e1c' + event.path.replace('/buy-api', ''));
+    const data = await response.json();
+    return {
+        statusCode: response.status,
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+};
