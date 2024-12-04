af527e384b3e2269d32a357ca0884a659c4e128a
Cleber Henrique <cleberschiavon@outlook.com>
2024-12-04 00:11:13 -0300

    changing node-fetch library to native fetch library

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,13 +1,33 @@
-const fetch = require('node-fetch');
+export async function handler(event) {
+    try {
+        const apiUrl = 'https://app.landingpage.com.br/api/checkoutloja/LPL2gc/5d87eb644e5631bc6a03f1e43a804e1c' + event.path.replace('/buy-api', '');
+        const response = await fetch(apiUrl);
 
-exports.handler = async (event) => {
-    const response = await fetch('https://app.landingpage.com.br/api/checkoutloja/LPL2gc/5d87eb644e5631bc6a03f1e43a804e1c' + event.path.replace('/buy-api', ''));
-    const data = await response.json();
-    return {
-        statusCode: response.status,
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-};
+        if (!response.ok) {
+            return {
+                statusCode: response.status,
+                body: JSON.stringify({ error: 'Failed to fetch data from API' }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+        }
+
+        const data = await response.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error', details: error.message }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    }
+}
