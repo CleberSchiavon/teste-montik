68a497b1dca968fa0b73002fa1b341f7f6ec0da2
Cleber Henrique <cleberschiavon@outlook.com>
2024-12-03 23:57:40 -0300

    fix(vite.config.ts): adding secure key and changing rewrite prop

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,8 +9,9 @@ export default defineConfig({
       '/buy-api': {
         target: 'https://app.landingpage.com.br/api/checkoutloja/LPL2gc/5d87eb644e5631bc6a03f1e43a804e1c',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        rewrite: (path) => path.replace(/^\/buy-api/, ''), 
       }
     }
   }
-})
+});
